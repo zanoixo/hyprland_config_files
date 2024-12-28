627aeee73397dbbb094b65d4087568d9993b0a12
zanoixo <62448782+zanoixo@users.noreply.github.com>
2024-12-28 11:58:38 +0100

    added laptop files

---
+++ laptop/move.sh
@@ -0,0 +1,2 @@
+cp -R /home/zan/.config/hypr /home/zan/.config/dolphinrc /etc/dunst /etc/xdg/waybar /home/zan/.config/rofi /home/zan/.config/kitty /usr/share/sddm/themes/Sweet-Ambar-Blue  /home/zan/Pictures/wallpapers/wall2_upscaled.jpeg ./
+
