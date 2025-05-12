6b9c49858ac2fe698f5f1036cfe6e283c57482d6
zanoixo <62448782+zanoixo@users.noreply.github.com>
2025-05-12 22:41:13 +0200

    added .config files for nvim and updated hyprland.conf

---
+++ nvim/init.lua
@@ -0,0 +1,46 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = "<Space>"
+vim.opt.number = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+
+local opts = {}
+require("lazy").setup("plugins")
+local builtin = require("telescope.builtin")
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+    ensure_installed = {"lua", "python", "c", "java"},
+    highlight = { enable = true },
+    indent = { enable = true },
+
+})
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
